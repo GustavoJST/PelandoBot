928311885b53838acfe1c082a7cf255f0dba79ce
GustavoJST <gutojusto22@gmail.com>
2022-12-01 00:33:21 -0300

    Added more database methods.

--- tgbot/utils/database.py
+++ tgbot/utils/database.py
@@ -4,16 +4,30 @@ from redis import asyncio as aioredis
 # Create a connection
 class Database:
     def __init__(self) -> None:
-        self.redis = aioredis.from_url("redis://localhost", decode_responses=True)
+        self.redis = aioredis.from_url("redis://localhost", decode_responses=True)      
     
-    async def add_user(self, chat_id):
-       await self.redis.rpush("users", chat_id)
+    async def set_user_state(self, chat_id):
+        # Se é já existe uma lista com os ids dos usuários ativos, por que guardar o estado do ID em outro lugar?
+        await self.redis.set(f"{chat_id}.state", "active")
+        await self.redis.rpush("active.users", chat_id)
+        
+    async def unset_user_state(self, chat_id):
+        await self.redis.set(f"{chat_id}.state", "inactive")
+        await self.redis.lrem("active.users", 1, chat_id)
         
     async def remove_user(self, chat_id):
        await self.redis.lrem(name="users", count=1, value=chat_id)
-    
-    async def get_current_users(self):
-        return await self.redis.lrange(name="users", start=0, end=-1)
+       
+    async def set_user_tags(self, chat_id, tags):
+        if self.redis.exists(f"{chat_id}.tags"):
+            await self.redis.delete(f"{chat_id}.tags")
+        await self.redis.rpush(f"{chat_id}.tags", tags)
+            
+    async def unset_user_tags(self, chat_id):
+        await self.redis.delete(f"{chat_id}.tags")
+        
+    async def get_active_users(self):
+        return await self.redis.lrange("active.users", 0, -1)
 
 # Instantiate database to be used across the project with import.
 db = Database()
