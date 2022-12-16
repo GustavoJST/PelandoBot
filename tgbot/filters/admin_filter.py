5f0ab84a12125f811093cb6f19731c8e8a4c47ce
GustavoJST <gutojusto22@gmail.com>
2022-12-16 15:28:25 -0300

    Modified admin_filter.py to allow the bot to be used in groups and private chats.

--- tgbot/filters/admin_filter.py
+++ tgbot/filters/admin_filter.py
@@ -1,13 +1,25 @@
 from telebot.asyncio_filters import SimpleCustomFilter
 from tgbot.models.users_model import Admin
+from telebot import types
 
 
 class AdminFilter(SimpleCustomFilter):
     """
-    Filter for admin users
+    Filter for admin users and private chats, since in a private chat,
+    the user has the 'member' attribute instead of 'administrator'.
     """
-
+    
     key = 'admin'
-    async def check(self, message):
+    
+    def __init__(self, bot):
+        self._bot = bot
 
-        return int(message.chat.id) == int(Admin.ADMIN.value)
+    async def check(self, message):
+        if message.chat.type in ["private"]:
+            return True
+        
+        elif isinstance(message, types.CallbackQuery):
+            result = await self._bot.get_chat_member(message.message.chat.id, message.from_user.id)
+            return result.status ('creator', 'administrator')
+        result = await self._bot.get_chat_member(message.chat.id, message.from_user.id)
+        return result.status in ['creator', 'administrator']
