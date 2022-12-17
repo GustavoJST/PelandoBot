d9776c6c2465eeb622dc3c8975ca56f8d2434280
GustavoJST <gutojusto22@gmail.com>
2022-12-17 01:03:28 -0300

    Deleted unused states.

--- tgbot/states/register_state.py
+++ tgbot/states/register_state.py
@@ -1,12 +1,9 @@
 # Create your states in this folder.
-
-
 from telebot.asyncio_handler_backends import State, StatesGroup
 
 
-class Register(StatesGroup):
+class UserStates(StatesGroup):
     """
     Group of states for registering
     """
-    name = State()
-    surname = State()
+    tags_button = State()
