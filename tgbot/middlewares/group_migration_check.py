069d272f90c109b428359803bec33e8549895a45
GustavoJST <gutojusto22@gmail.com>
2022-12-19 21:46:55 -0300

    Added group id migration check middleware.

---
+++ tgbot/middlewares/group_migration_check.py
@@ -0,0 +1,26 @@
+from telebot.asyncio_handler_backends import BaseMiddleware
+from telebot.async_telebot import AsyncTeleBot
+from telebot.types import Message
+from tgbot.utils.database import async_db
+
+class GroupMigrationMiddleware(BaseMiddleware):
+    def __init__(self, bot: AsyncTeleBot) -> None:
+        # Always specify update types, otherwise middlewares won't work.
+        self.update_types = ['message']
+        self.bot = bot
+
+    async def pre_process(self, message: Message, data):
+        if message.migrate_to_chat_id != None:
+            old_id = message.chat.id
+            new_id = message.migrate_to_chat_id
+            for state in ["tags.", "state."]:
+                if await async_db.redis.exists(f"{state}" + f"{old_id}"):
+                    await async_db.redis.rename(f"{state}" + f"{old_id}" , f"{state}" + f"{new_id}")
+   
+    # Don't delete this or it'll raise NotImplementedError.      
+    async def post_process(self, message, data, exception):
+        pass
+            
+            
+            
+        
