f107bd32c7a6f1e7485e60ee15167caeadc8df05
GustavoJST <gutojusto22@gmail.com>
2022-12-21 22:32:57 -0300

    Modified bot.py to use webhook system with aiohttp.

--- bot.py
+++ bot.py
@@ -1,20 +1,23 @@
 import asyncio
 import promotion_query
 import logging
+import ssl
+from aiohttp import web
 from telebot import logger
+from multiprocessing import Process
 
-# Types
-from telebot.types import BotCommand, BotCommandScopeAllPrivateChats
+# Types.
+from telebot.types import BotCommand, BotCommandScopeAllPrivateChats, Update
 
-# Filters
+# Filters.
 from tgbot.filters.admin_filter import AdminFilter 
 
-# States
+# States.
 from telebot.asyncio_storage import StateRedisStorage
 from telebot.asyncio_filters import StateFilter
 from tgbot.states.register_state import UserStates
 
-# Handlers
+# Handlers.
 from tgbot.handlers.admin import admin_user
 from tgbot.handlers.spam_command import anti_spam
 from tgbot.handlers.start import start
@@ -23,26 +26,44 @@ from tgbot.handlers.stop import stop
 from tgbot.handlers.promo import promo
 from tgbot.handlers.help import help
 
-# Callbacks
+# Callbacks.
 from tgbot.callbacks.callback_tags import tag_option_handler, handle_tags_input
 
-# Middlewares
+# Middlewares.
 from tgbot.middlewares.antiflood_middleware import AntiFloodMiddleware
 from tgbot.middlewares.group_migration_check import GroupMigrationMiddleware
 
-# Synchronous database
+# Synchronous database.
 from tgbot.utils.database import sync_db
 
-# Telebot
+# Telebot.
 from telebot.async_telebot import AsyncTeleBot
 
-# Config
-from tgbot import config
+# Config.
+from tgbot.config import TOKEN, HOST
+
+WEBHOOK_HOST = HOST
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open').
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP address.
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST.
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate.
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key.
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(TOKEN)
 
 CHAT_TYPES = ["private", "group", "supergroup"]
 
-bot = AsyncTeleBot(config.TOKEN, state_storage=StateRedisStorage(prefix="state."))
+bot = AsyncTeleBot(TOKEN, state_storage=StateRedisStorage(prefix="state."))
+logger = logger
 logger.setLevel(logging.DEBUG)
+
 
 # Cleans database on bot startup.
 def clean_db():
@@ -59,8 +80,29 @@ def clean_db():
     users_id = sync_db.redis.smembers("active.chats.id")
     for user_id in users_id:
         sync_db.redis.delete(f"state.{user_id}")
-    
-async def bot_run():
+        
+
+# Process webhook calls.
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = Update.de_json(request_body_dict)
+        asyncio.ensure_future(bot.process_new_updates([update]))
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+
+# Remove webhook and closing session before exiting.
+async def shutdown(app):
+    logger.info('Shutting down: removing webhook')
+    await bot.remove_webhook()
+    logger.info('Shutting down: closing session')
+    await bot.close_session()
+
+
+async def setup():
+    # Message handlers and callbacks.
     def register_handlers():
         bot.register_message_handler(start, commands=['start'], chat_types=CHAT_TYPES, admin=True, 
                                      pass_bot=True, content_types=["text"])
@@ -77,7 +119,7 @@ async def bot_run():
         bot.register_message_handler(tags, commands=['tags'], chat_types=CHAT_TYPES, 
                                      admin=True, pass_bot=True, content_types=["text"])
         
-        bot.register_callback_query_handler(callback=tag_option_handler, pass_bot=True, chat_types=CHAT_TYPES,
+        bot.register_callback_query_handler(callback=tag_option_handler, pass_bot=True,
                                             func=lambda call: True, state=UserStates.tags_button)
         
         bot.register_message_handler(handle_tags_input, chat_types=CHAT_TYPES, admin=True, pass_bot=True,
@@ -91,19 +133,39 @@ async def bot_run():
                                         BotCommand("tags", "Adicionar/remover tags")],
                                         scope=BotCommandScopeAllPrivateChats())
 
-    # Middlewares
+    # Middlewares.
     bot.setup_middleware(AntiFloodMiddleware(limit=2, bot=bot))
     bot.setup_middleware(GroupMigrationMiddleware(bot))
 
-    # custom filters
+    # Custom filters.
     bot.add_custom_filter(AdminFilter(bot))
     bot.add_custom_filter(StateFilter(bot))
     
-    await bot.polling(non_stop=True)
+    # Remove webhook, it fails sometimes the set if there is a previous webhook.
+    logger.info('Starting up: removing old webhook')
+    await bot.remove_webhook()
+    
+    # Set webhook.
+    logger.info('Starting up: setting webhook')
+    await bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+    app = web.Application()
+    app.router.add_post('/{token}/', handle)
+    app.on_cleanup.append(shutdown)
+    return app
 
-async def main():
+
+if __name__ == '__main__':
     clean_db()
-    await asyncio.gather(bot_run(), promotion_query.get_promotions())   
-        
-if __name__ == "__main__":
-    asyncio.run(main())
+    promotion_query_process = Process(target=promotion_query.prepare_query)
+    promotion_query_process.start() 
+    # Build ssl context.
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+    # Start aiohttp server.
+    web.run_app(
+        setup(),
+        host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=context,
+    )
