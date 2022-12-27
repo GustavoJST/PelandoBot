12cc6a4a10e86c8e12ee0624eff655e2afbfec1b
GustavoJST <gutojusto22@gmail.com>
2022-12-27 18:00:10 -0300

    Modified setup files.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        version='1.0.0',
+        url='https://github.com/GustavoJST/PelandoBot',
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+    )
